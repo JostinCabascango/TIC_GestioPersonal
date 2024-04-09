54ca904c9a781c819de1bb26bc45f3388163cab5
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2024-04-09 19:49:17 +0200

    Crear Modelos

--- Centre/models.py
+++ Centre/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    lastname1 = models.CharField(max_length=100)
+    lastname2 = models.CharField(max_length=100)
+    email = models.EmailField()
+    course = models.CharField(max_length=100)
+    modules = models.TextField()
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    lastname1 = models.CharField(max_length=100)
+    lastname2 = models.CharField(max_length=100)
+    email = models.EmailField()
+    course = models.CharField(max_length=100)
+    tutor = models.BooleanField(default=False)
+    modules = models.TextField()
