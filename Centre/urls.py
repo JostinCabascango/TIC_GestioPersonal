2bd4fbafbc09479c614a575dc4830f0fd887daed
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2024-04-09 19:48:42 +0200

    Definir todas las urls para la p1

--- Centre/urls.py
+++ Centre/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.hello, name="hello"),
     path("about", views.about, name="about"),
-    path("index", views.index, name="index")
+    path("index", views.index, name="index"),
+    path("students", views.students, name="students"),
+    path("students/<int:student_id>", views.student_detail, name="student_detail"),
+    path("teachers", views.teachers, name="teachers"),
+    path("teachers/<int:teacher_id>", views.teacher_detail, name="teacher_detail"),
 ]
